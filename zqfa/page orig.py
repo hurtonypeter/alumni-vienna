1aa8dfb0a21c6134d87fc9430edbb9b65fc15ee4
QFin Club <qfinclub@wu.ac.at>
2015-10-10 19:35:49 +0000

    test

---
+++ zqfa/page orig.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, render_template, session, redirect, url_for
+from flask.ext.login import current_user, logout_user, login_required
+
+bp = Blueprint('page', __name__)
+
+
+@bp.route('/')
+def start():
+    if current_user.is_authenticated() and current_user.registered:
+        return redirect(url_for('page.home'))
+    elif ((current_user.is_authenticated() and not current_user.registered)
+          or session.get('registration') == 1
+          or session.get('registration') == 2):
+        logout_user()
+        session['registration'] = None
+    return home()
+
+@bp.route('/home')
+def home():
+    return render_template('page/home.html')
+
+@bp.route('/about')
+def about():
+    return render_template('page/about.html')
+
+@bp.route('/qf')
+def qf():
+    return render_template('page/qf.html')
+
+@bp.route('/advisory')
+def advisory():
+    return render_template('page/advisory.html')
+
+@bp.route('/terms')
+def terms():
+    return render_template('page/terms.html')
+
+@bp.route('/privacy')
+def privacy():
+    return render_template('page/privacy.html')
+
+@bp.route('/success')
+@login_required
+def success():
+    return render_template('page/success.html')
