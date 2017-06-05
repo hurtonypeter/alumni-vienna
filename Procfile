25443207c46123147e7b36d63bcccd20e46f30e0
Peter Hurtony <hurtonypeter@gmail.com>
2017-06-06 00:09:58 +0200

    dyno stripping

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
-web: newrelic-admin run-program uwsgi uwsgi.ini
+web: uwsgi uwsgi.ini
 clock: python -u start_clock.py
-worker: python -u start_worker.py
--- start_clock.py
+++ start_clock.py
@@ -1,8 +1,8 @@
-from zqfa.worker.clock import queue, sched
+from zqfa.worker.clock import sched
 from zqfa.worker.tasks import send_newsletter
 
 @sched.scheduled_job('interval', seconds=10)
 def timed_job():
-    queue.enqueue(send_newsletter)
+    send_newsletter()
 
 sched.start()
